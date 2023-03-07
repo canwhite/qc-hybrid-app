00f5c35d396ef567dafc8514bd950ccf43182c5f
qiaochao <qiaochao@icourt.cc>
2023-03-07 20:52:37 +0800

    feat : android webview

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,29 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    //外置一个webview
+    private  WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //挂载视图内容
+        setContentView(R.layout.activity_main);
+
+        //先挂载好webview
+        webview = (WebView) findViewById(R.id.web_view);
+
+
+    }
+}
