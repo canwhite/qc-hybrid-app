745e07c10fb7411a2d54fdf6404ed4bae5eb673d
qiaochao <qiaochao@icourt.cc>
2023-03-08 00:48:08 +0800

    feat : des

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -14,32 +14,6 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends BridgeActivity {
-    //外置一个webview
-    private  WebView webview;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //挂载视图内容
-        setContentView(R.layout.activity_main);
-
-        //先挂载好webview
-        webview = (WebView) findViewById(R.id.web_view);
-                //设置允许弹框
-                webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-                webview.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-                //当从一个网页跳转到另一个网页时，
-                //我们希望目标网页仍然在当前webview显示
-                webview.setWebViewClient(new WebViewClient());
-        //设置属性
-        webview.getSettings().setJavaScriptEnabled(true);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // 在API 21及以上版本中，需要使用以下代码才能支持HTTP：
-            webview.enableSlowWholeDocumentDraw();
-            webview.loadUrl("http://192.168.10.61:8081/");
-            //webview.loadUrl("file:///android_asset/public/index.html");
-        }
-        
-    }
-}
+//点击继承类BridgeActivity，然后在内部找到Bridge，然后在Bridge里就可以找到webview
+//不用再自己新建继承，在那个地方改就可以
+public class MainActivity extends BridgeActivity {}
