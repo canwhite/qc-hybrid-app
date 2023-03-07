e91f164e99562313113520d0f9ebf52413ec2a52
liyafei <liyafei@csii.com.cn>
2023-03-07 23:23:00 +0800

    publicpath

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: liyafei liyafei@csii.com.cn
+ * @Date: 2023-03-07 23:05:58
+ * @LastEditors: liyafei liyafei@csii.com.cn
+ * @LastEditTime: 2023-03-07 23:19:37
+ * @FilePath: /qc-hybrid-app/vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  publicPath:'./'
+}
